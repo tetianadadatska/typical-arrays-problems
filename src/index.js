c88f04bf2c0545eb915397e6740c37222bfbb3c7
Tetiana Dadatska <tatyanadadatska@gmail.com>
2021-03-16 00:20:03 +0100

    feat: implementing typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (typeof array == 'undefined' || !array.length) {
+        return 0;
+    }
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (typeof array == 'undefined' || !array.length) {
+        return 0;
+    }
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (typeof array == 'undefined' || !array.length) {
+        return 0;
+    }
+    let sum =0;
+    for (var i=0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / i;
 }
